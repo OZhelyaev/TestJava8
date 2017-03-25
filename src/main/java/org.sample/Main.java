493fb9daac2ac5ae41ea2170ebd63769199c2558
OZhelyaev <ozhelyaev@gmail.com>
2017-03-26 00:14:35 +0300

    (#1) initialization gradle project

---
+++ src/main/java/org.sample/Main.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by OZhelyaev on 25.03.2017.
+ */
+public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("TEST");
+    }
+}
---
+++ src/test/java/org.sample/MainTest.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by OZhelyaev on 25.03.2017.
+ */
+public class MainTest {
+    @Test
+    public void mainMethodTest() throws Exception {
+      //  Assert.assertTrue(1==1);
+    }
+
+}
